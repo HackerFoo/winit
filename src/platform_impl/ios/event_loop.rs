a2ff2f8a263c776c3d9934d459f0b9fb484bd094
Dusty DeWeese <dustin.deweese@gmail.com>
2021-09-03 17:57:01 -0700

    disable loop exit
    Triggered when scrolling in subviews on iOS

--- src/platform_impl/ios/event_loop.rs
+++ src/platform_impl/ios/event_loop.rs
@@ -255,7 +255,7 @@ fn setup_control_flow_observers() {
                 #[allow(non_upper_case_globals)]
                 match activity {
                     kCFRunLoopBeforeWaiting => app_state::handle_main_events_cleared(),
-                    kCFRunLoopExit => unimplemented!(), // not expected to ever happen
+                    kCFRunLoopExit => {} //unimplemented!(), // not expected to ever happen
                     _ => unreachable!(),
                 }
             }
@@ -271,7 +271,7 @@ fn setup_control_flow_observers() {
                 #[allow(non_upper_case_globals)]
                 match activity {
                     kCFRunLoopBeforeWaiting => app_state::handle_events_cleared(),
-                    kCFRunLoopExit => unimplemented!(), // not expected to ever happen
+                    kCFRunLoopExit => {} //unimplemented!(), // not expected to ever happen
                     _ => unreachable!(),
                 }
             }
